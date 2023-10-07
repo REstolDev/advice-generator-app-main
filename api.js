0e03a0d3df7b7a0b16008481423a01672def16cd
Ramon Estol <ramonestolgarcia@gmail.com>
2023-10-07 10:20:59 +0200

    readme  and responsive divider

--- api.js
+++ api.js
@@ -1,27 +1,41 @@
 function getAdvice() {
-    const adviceSpan = document.getElementById("adviceTxt");
-    const adviceId = document.getElementById("adviceId");
+  const adviceSpan = document.getElementById("adviceTxt");
+  const adviceId = document.getElementById("adviceId");
 
-    fetch("https://api.adviceslip.com/advice")
-        .then(response => {
-            if (!response.ok) {
-                throw new Error(`The request was not successful. Status: ${response.status}`);
-            }
-            return response.json();
-        })
-        .then(data => {
+  fetch("https://api.adviceslip.com/advice")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error(
+          `The request was not successful. Status: ${response.status}`
+        );
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const advice = data.slip.advice;
+      const id = data.slip.id;
 
-            const advice = data.slip.advice;
-            const id = data.slip.id;
-
-            adviceSpan.textContent = advice;
-            adviceId.textContent = "#" + id;
-        })
-        .catch(error => {
-            console.error("Error getting advice:", error);
-        });
+      adviceSpan.textContent = advice;
+      adviceId.textContent = "#" + id;
+    })
+    .catch((error) => {
+      console.error("Error getting advice:", error);
+    });
 }
 document.addEventListener("DOMContentLoaded", getAdvice);
 
 const getAdviceBtn = document.getElementById("adviceBtn");
 getAdviceBtn.addEventListener("click", getAdvice);
+
+const divider = document.getElementById("divider");
+const mediaQuery = window.matchMedia("(min-width: 600px)");
+
+
+function changeDivider(mediaQuery) {
+  mediaQuery.matches
+    ? (divider.src = "images/pattern-divider-desktop.svg")
+    : (divider.src = "images/pattern-divider-mobile.svg");
+}
+
+mediaQuery.addEventListener("change", changeDivider);
+changeDivider(mediaQuery);
